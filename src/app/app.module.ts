6fc7d668fc9c7960a880da57fc8dc48abe5e065d
Donovan Clarke <donovanclarketx@gmail.com>
2017-11-05 22:43:06 -0500

    routing wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },{
+        path: 'heroes',
+        component: HeroesComponent
+      },{
+        path: 'dashboard',
+        component: DashboardComponent
+      },{
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   bootstrap: [
     AppComponent
@@ -23,4 +43,5 @@ import { HeroService } from './hero.service';
     HeroService
   ]
 })
+
 export class AppModule { }
